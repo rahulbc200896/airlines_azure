daa64ebf7026c89b5e1f12e4b46ca01b47441413
rahulbc200896 <rahulbc17@gmail.com>
2025-08-16 18:14:08 +0530

    Update application.py

--- application.py
+++ application.py
@@ -44,4 +44,5 @@ def predict_ticket_price():
         raise Airlines_Exeption(e,sys)
 
 if __name__ =='__main__':
-    app.run(debug=True)
+    port= int(os.environ.get("PORT",8080))
+    app.run(host="0.0.0.0",port=port)
